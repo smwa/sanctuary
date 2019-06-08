0f89c2f4515ed29411ea7e7739d14f083a0abb99
Michael Smith <michael.smith.ok@gmail.com>
2019-06-08 15:01:03 -0500

    Chat GET complete, and possibly POST

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import JsonResponse 
+from chat.models import Message
+from time import sleep
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == "GET":
+        lastSeenId = int(request.GET.get('lastSeenId', '0'))
+        messages = Message.objects.all().filter(id__gt=lastSeenId)
+
+        longPollingSecondsRemaining = 5.0
+        SLEEP_INTERVAL = 0.5
+        while len(messages) < 1 and longPollingSecondsRemaining > 0.0:
+          sleep(SLEEP_INTERVAL)
+          longPollingSecondsRemaining -= SLEEP_INTERVAL
+          messages = Message.objects.all().filter(id__gt=lastSeenId)
+
+        POPMessages = map(lambda m: {'id': m.id, 'sender': m.sender, 'body': m.body}, messages)
+        return JsonResponse({'messages': list(POPMessages)})
+    if request.method == "POST":
+        sender = request.POST.get('sender', None)
+        body = request.POST.get('body', None)
+        if sender is None or body is None:
+            return JsonResponse("Sender and Body are required fields", status=400)
+        message = Message(sender=sender, body=body)
+        message.save()
+        return JsonResponse('Ok')
